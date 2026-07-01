2467cac82a99dc3e125a371d894ba803a98013c7
Karina Aparecida de Souza <61884002+KarinaPadilha2512@users.noreply.github.com>
2026-07-01 14:14:08 -0300

    fix: log unhandled exceptions in GlobalExceptionHandler
    Added SLF4J logger field and log.error call to capture unhandled
    exceptions that would otherwise be invisible in application logs.

--- src/main/java/com/totvus/payments/web/GlobalExceptionHandler.java
+++ src/main/java/com/totvus/payments/web/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.totvus.payments.web;
 import com.totvus.payments.domain.exception.DomainException;
 import com.totvus.payments.domain.exception.RecursoNaoEncontradoException;
 import com.totvus.payments.domain.exception.TransicaoEstadoInvalidaException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.validation.FieldError;
@@ -15,6 +17,8 @@ import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ExceptionHandler(RecursoNaoEncontradoException.class)
     public ProblemDetail handleNaoEncontrado(RecursoNaoEncontradoException ex) {
@@ -43,6 +47,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ProblemDetail handleGenerico(Exception ex) {
+        log.error("Unhandled exception", ex);
         return ProblemDetail.forStatusAndDetail(HttpStatus.INTERNAL_SERVER_ERROR,
             "Erro interno do servidor");
     }
